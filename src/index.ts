5b3e272d01cdf47bca8cc7638128888b2351985b
Naveed Akhtar <naveed.akhtar@ketch.com>
2023-06-07 15:51:19 +0530

    chore: cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,9 @@
-import { getCookie } from '@ketch-com/ketch-cookie'
-
 /**
  * Get a value from a cookie by the key.
  *
  * @param w The window object
  * @param key The cookie key
  */
-export default function getConsent(w: Window): any {
-  const value = getCookie(w, '_swb_ketch_')
-    if (value) {
-      try {
-        return JSON.parse(atob(value))
-      } catch (e) {
-        return
-      }
-    }
-    return
+export default function getConsent(): any {
+  return { foo: 'bar' }
 }
